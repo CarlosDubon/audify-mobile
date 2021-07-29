e5075d73135b37e8854798cc7c819c3817085a78
Douglas Hernández <00004516@uca.edu.sv>
2021-07-29 15:01:33 -0600

    Add: new position smooth moving

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,26 +1,29 @@
-import React, { useEffect, useState } from "react";
-import { Image, Pressable, StyleSheet, View } from "react-native";
-import MapView, { Circle, Marker } from "react-native-maps";
-import Geolocation from "react-native-geolocation-service";
-import Icon from "react-native-vector-icons/FontAwesome";
-import { colors } from "../theme/colors";
-import { connect } from "react-redux";
-import UserPreferences from "../modals/UserPreferences";
-import axios from "axios";
-import PlacesContainer from "../components/PlacesContainer";
-import { io } from "socket.io-client";
-import CompassHeading from "react-native-compass-heading";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
+import { Image, Pressable, StyleSheet, View } from 'react-native';
+import MapView, { Circle, Marker } from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { colors } from '../theme/colors';
+import { connect } from 'react-redux';
+import UserPreferences from '../modals/UserPreferences';
+import axios from 'axios';
+import PlacesContainer from '../components/PlacesContainer';
+import { io } from 'socket.io-client';
+import CompassHeading from 'react-native-compass-heading';
+import { getDistance, getRhumbLineBearing } from 'geolib';
+import { getEarthAngleFromArc, getGeoVelocityComponents, getNewPosition } from '../utils';
 
 const MapPage = (props) => {
-  const [myPosition, setMyPosition] = useState(null);
-  const [audioType,setAudioType]=useState(1)
-  const [show,setShow] = useState(false)
-  const [enabled,setEnabled] =useState(false)
-  const [places,setPlaces] = useState([])
-
-  const [update, setUpdate] = useState(false);
+  const [audioType,setAudioType] = useState(1);
+  const [show,setShow] = useState(false);
+  const [places,setPlaces] = useState([]);
   const [selectedPlaces, setSelectedPlaces] = useState([]);
-
+  
+  const [enabled,setEnabled] = useState(false);
+  const [update, setUpdate] = useState(false);
+  
+  const [myPosition, setMyPosition] = useState(null);
   const [compassHeading, setCompassHeading] = useState(0);
 
   useEffect(() => {
@@ -28,11 +31,90 @@ const MapPage = (props) => {
 
     CompassHeading.start(degree_update_rate, ({heading, accuracy}) => {
       setCompassHeading(heading);
-    })
+    });
 
     return () => {
       CompassHeading.stop();
     };
+  }, []);
+
+  useEffect(() => {
+    const SECONDS = 1; //Time in seconds
+    const MIN_DISTANCE = 1; //Meters
+    
+    const SHORT_DISTANCE = 10;
+    const MEDIUM_DISTANCE = 35;
+    const LARGE_DISTANCE = 75;
+
+    const DELTA_SHORT = 1.3;
+    const DELTA_MEDIUM = 2.22;
+    const DELTA_LARGE = 4.15;
+
+    const positionInterval = setInterval(() => {
+      Geolocation.getCurrentPosition(position => {
+        if (!myPosition) {
+          setMyPosition({
+            longitude: position.coords.longitude,
+            latitude: position.coords.latitude,
+            latitudeDelta: 0.0009,
+            longitudeDelta: 0.0009,
+          });
+          return;
+        }
+
+        const current = {
+          longitude: position.coords.longitude,
+          latitude: position.coords.latitude,
+        };
+
+        const previous = {
+          longitude: myPosition.longitude,
+          latitude: myPosition.latitude,
+        };
+
+        const distanceDiff = getDistance(previous, current, 0.01);
+
+        if (distanceDiff < MIN_DISTANCE) { return; }
+        if (distanceDiff > LARGE_DISTANCE) {
+          setMyPosition({
+            longitude: position.coords.longitude,
+            latitude: position.coords.latitude,
+            latitudeDelta: 0.0009,
+            longitudeDelta: 0.0009,
+          });
+          return;
+        }
+
+        let deltaMeters = 0;
+
+        if (distanceDiff > MIN_DISTANCE && distanceDiff <= SHORT_DISTANCE) {
+          deltaMeters = DELTA_SHORT;
+        }
+
+        if (distanceDiff > SHORT_DISTANCE && distanceDiff <= MEDIUM_DISTANCE) {
+          deltaMeters = DELTA_MEDIUM;
+        }
+
+        if (distanceDiff > MEDIUM_DISTANCE && distanceDiff <= LARGE_DISTANCE) {
+          deltaMeters = DELTA_LARGE;
+        }
+
+        const angle = getRhumbLineBearing(previous, current);
+        const deltaArc = getEarthAngleFromArc(deltaMeters);
+        const velocityComponents = getGeoVelocityComponents(deltaArc, SECONDS, angle);
+
+        const newPosition = getNewPosition(previous, velocityComponents, SECONDS);
+
+        setMyPosition({
+          ...newPosition,
+          latitudeDelta: 0.0009,
+          longitudeDelta: 0.0009,
+        });
+      }, error => { console.log(error); },
+      { enableHighAccuracy: true, distanceFilter: 1 });
+    }, SECONDS * 1000);
+
+    return clearInterval(positionInterval);
   }, []);
 
   useEffect(() => {
@@ -69,29 +151,29 @@ const MapPage = (props) => {
   }, []);
 
   useEffect(() => {
-    let socket = io("http://147.182.171.70",{
+    let socket = io('http://147.182.171.70',{
       auth: {
-        token: props.token
+        token: props.token,
       },
       autoConnect:true,
-      path:"/api/socket.io"
+      path:'/api/socket.io',
 
     });
-    console.log(socket)
-    socket.on("connect", () => {
+    console.log(socket);
+    socket.on('connect', () => {
       console.log(socket.id); // x8WIv7-mJelg7on_ALbx
     });
-    socket.on("speaker_update",(data)=>{
-      if(data){
-        console.log("Tick!")
+    socket.on('speaker_update',(data)=>{
+      if (data){
+        console.log('Tick!');
         fetchPlaces();
       }
 
     });
 
-    return()=>{
-      socket.disconnect()
-    }
+    return ()=>{
+      socket.disconnect();
+    };
 
   }, []);
 
@@ -104,21 +186,21 @@ const MapPage = (props) => {
   };
 
 
-  const fetchPlaces=async ()=>{
+  const fetchPlaces = async ()=>{
     try {
       let res = await axios.get(`${props.server}/speaker`,{
         headers:{
-          "Authorization":`Bearer ${props.token}`
-        }
-      })
-      setPlaces(res.data)
-    }catch (e) {
-      console.log(e)
+          'Authorization':`Bearer ${props.token}`,
+        },
+      });
+      setPlaces(res.data);
+    } catch (e) {
+      console.log(e);
     }
-  }
-  const getReferencePlace=(id)=>{
-    return places.find(p=>p._id===id)
-  }
+  };
+  const getReferencePlace = (id)=>{
+    return places.find(p=>p._id === id);
+  };
   if (!myPosition) {
     return <></>;
   }
@@ -134,24 +216,24 @@ const MapPage = (props) => {
       >
         {
           selectedPlaces.map((mPlace, i) => {
-            const place = getReferencePlace(mPlace)
-            if(!place){
-              return <></>
+            const place = getReferencePlace(mPlace);
+            if (!place){
+              return <></>;
             }
-            return(
+            return (
               <View key={place._id}>
                 <Marker
 
                   coordinate={{ latitude: place.latitude, longitude: place.longitude }}>
-                  <Image source={require("../theme/images/audify.png")} style={{ width: 20, height: 20 }} />
+                  <Image source={require('../theme/images/audify.png')} style={{ width: 20, height: 20 }} />
                 </Marker>
 
                 <Circle center={{ latitude: place.latitude, longitude: place.longitude }}
                         radius={place.radius}
-                        fillColor={"rgba(45, 52, 54,0.3)"}
-                        strokeColor={"rgba(45, 52, 54,0.5)"} />
+                        fillColor={'rgba(45, 52, 54,0.3)'}
+                        strokeColor={'rgba(45, 52, 54,0.5)'} />
               </View>
-              )
+              );
 
         })
         }
@@ -161,7 +243,7 @@ const MapPage = (props) => {
       </MapView>
       <View style={Style.preferencesBtn}>
         <Pressable onPress={()=>setShow(true)}>
-          <Icon name={"gear"} size={30} color={colors.light} />
+          <Icon name={'gear'} size={30} color={colors.light} />
         </Pressable>
       </View>
       <UserPreferences show={show} onClose={()=>setShow(false)} onValueChange={setAudioType} value={audioType} />
@@ -174,37 +256,37 @@ const MapPage = (props) => {
 
 const Style = StyleSheet.create({
   container: {
-    position: "absolute",
+    position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
     bottom: 0,
-    justifyContent: "flex-end",
-    alignItems: "center",
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
   map: {
-    position: "absolute",
+    position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
     bottom: 0,
   },
   preferencesBtn:{
-    position:"absolute",
+    position:'absolute',
     top:0,
     right:0,
     backgroundColor:colors.primary,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     borderRadius:100,
     margin:8,
     height:50,
-    width:50
-  }
+    width:50,
+  },
 });
-const mapStateToProps=(state)=>({
+const mapStateToProps = (state)=>({
   token:state.user.token,
   server:state.config.server,
-  socket:state.config.socket
-})
-export default connect(mapStateToProps) (MapPage);
+  socket:state.config.socket,
+});
+export default connect(mapStateToProps)(MapPage);
--- src/utils.js
+++ src/utils.js
@@ -3,16 +3,12 @@ const convertRange = ( value, r1, r2 ) => {
 };
 
 const clamp = (num, min, max) => {
-  return Math.min(Math.max(num, min), max)
-}
-export const getVolumeFromLinearDistance = (d,max) => {
-  const converted = convertRange(d, [max,0], [0,1]);
-  return clamp(converted, 0, 1);
-}
+  return Math.min(Math.max(num, min), max);
+};
 
-const expFunction=(d,max)=>{
-  return Math.exp(-d/max)
-}
+const expFunction = (d,max)=>{
+  return Math.exp(-d / max);
+};
 
 /**
  * @param tetha user compass reference (North)
@@ -21,27 +17,65 @@ const expFunction=(d,max)=>{
 const rotateRefAngle = (tetha, alpha) => {
   const beta = alpha - tetha;
   return beta;
-}
+};
 
-const degrees_to_radians=(degrees)=>
+const degreesToRadians = (degrees)=>
 {
-  var pi = Math.PI;
-  return degrees * (pi/180);
-}
+  const pi = Math.PI;
+  return degrees * (pi / 180);
+};
+
+const radiansToDegrees = (radians) => {
+  const pi = Math.PI;
+  return radians * ( 180 / pi );
+};
+
+export const getEarthAngleFromArc = (distance) => {
+  const EarthRadius = 6371 * 1000;
+
+  //Arc = R * theta -> theta = Arc / R -> Radians
+  const theta = distance / EarthRadius;
+  return radiansToDegrees(theta);
+};
+
+export const getGeoVelocityComponents = (distance, time, theta) => {
+  const alpha = degreesToRadians(theta);
+  const velocity = distance / time;
+  const vx = velocity * Math.sin(alpha);
+  const vy = velocity * Math.cos(alpha);
+
+  return {
+    v: velocity,
+    vx: vx,
+    vy: vy,
+  };
+};
+
+export const getNewPosition = ({ latitude, longitude }, { vx, vy }, t) => {
+  return {
+    latitude: latitude + (vy * t),
+    longitude: longitude + (vx * t),
+  };
+};
 
 export const getBalanceFromAngles = (mCompassHeading, placeHeading) => {
-  const placeRotatedAngle = degrees_to_radians(
+  const placeRotatedAngle = degreesToRadians(
     rotateRefAngle(mCompassHeading, placeHeading)
   );
 
   return Math.sin(placeRotatedAngle);
-}
+};
 
-export const getVolumeFromExpDistance=(d,max)=>{
-  const nMax = expFunction(max,max)
-  const nMin = expFunction(0,max)
-  const nDistance= expFunction(d,max)
+export const getVolumeFromLinearDistance = (d,max) => {
+  const converted = convertRange(d, [max,0], [0,1]);
+  return clamp(converted, 0, 1);
+};
+
+export const getVolumeFromExpDistance = (d,max)=>{
+  const nMax = expFunction(max,max);
+  const nMin = expFunction(0,max);
+  const nDistance = expFunction(d,max);
 
   const converted = convertRange(nDistance, [nMax,nMin], [0,1]);
   return clamp(converted, 0, 1);
-}
+};
